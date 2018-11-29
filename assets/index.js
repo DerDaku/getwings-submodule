9e985161426861f81c79c87352a0268e91f9524d
Malte Boeing <hello@malte.nz>
2018-11-29 16:32:42 +0100

    added keys into submodule

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,15 @@
 import * as units from './units'
 import * as colors from './colors'
 import * as helpers from './helpers'
-import * as keys from './keys'
 import * as images from './images'
 import * as lib from './lib'
 import * as sound from './sound'
 
 export { default as stylesJs } from './stylesJs'
+export { default as keys } from './keys'
 export { units }
 export { colors }
 export { helpers }
-export { keys }
 export { images }
 export { lib }
 export { sound }
