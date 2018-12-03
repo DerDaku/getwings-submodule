6c547bf184689a2e14c2a1b2b15d6c63f12de093
Malte Boeing <hello@malte.nz>
2018-12-03 16:45:12 +0100

    updated palette.js helper

--- assets/helpers/palette.js
+++ assets/helpers/palette.js
@@ -3,17 +3,36 @@ import * as colors from '../colors'
 const palette = id => {
   switch (id) {
     case 2:
+      // name: Alltag
+      // category_name: Kurzmeditationen
       return {
         gradient: colors.gradientAlpha,
         color: colors.alpha2,
       }
     case 3:
+      // name: Arbeit & Leistungsfähigkeit
+      // category_name: Kurzmeditationen
       return {
         gradient: colors.gradientFoxtrot,
         color: colors.foxtrot,
       }
-    case 4:
-      // kindermedtationen
+    case 5:
+      // name: Arbeit & Leistungsfähigkeit
+      // category_name: Trancemeditationen
+      return {
+        gradient: colors.gradientFoxtrot,
+        color: colors.foxtrot,
+      }
+    case 6:
+      // name: Gesundheit
+      // category_name: Trancemeditationen
+      return {
+        gradient: colors.gradientBravo,
+        color: colors.bravo,
+      }
+    case 7:
+      // name: Kindermedtationen
+      // category_name: Kindermedtationen
       return {
         gradient: colors.gradientCharlie,
         color: colors.charlie,
