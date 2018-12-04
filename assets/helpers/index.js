a3ce2ecedb579efa7ce4f04000a7f3eff5afe2ee
Malte Boeing <hello@malte.nz>
2018-12-04 07:11:02 +0100

    added a new uppercase helper

--- assets/helpers/index.js
+++ assets/helpers/index.js
@@ -4,5 +4,14 @@ import parentCategoryUrl from './parentCategoryUrl'
 import requiredInput from './requiredInput'
 import validEmail from './validEmail'
 import lastIndexOf from './lastIndexOf'
+import upperCaseFirstChar from './upperCaseFirstChar'
 
-export { heading, palette, parentCategoryUrl, requiredInput, validEmail, lastIndexOf }
+export {
+  heading,
+  palette,
+  parentCategoryUrl,
+  requiredInput,
+  validEmail,
+  lastIndexOf,
+  upperCaseFirstChar,
+}
